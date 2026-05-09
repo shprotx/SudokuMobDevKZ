49a5aa1cdfac0544eb67ba8b70301155318263bd
shprotx <shprot2x@gmail.com>
2026-05-09 21:18:40 +0500

    REFACTOR: SolvedGridAnimation + данные пазла → core/uicommon/sudokuanim/

---
+++ app/src/main/java/ru/shprot/sudokumobdevkz/core/uicommon/sudokuanim/AnimatedSolvedGrid.kt
@@ -0,0 +1,40 @@
+package ru.shprot.sudokumobdevkz.core.uicommon.sudokuanim
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
+
+private const val INITIAL_DELAY_MS = 400L
+private const val CELL_DELAY_MS = 25L
+
+@Composable
+fun AnimatedSolvedGrid(
+    modifier: Modifier,
+) {
+    var visibleCells by remember { mutableStateOf(SolvedPuzzleData.INITIAL_FILLED) }
+
+    LaunchedEffect(Unit) {
+        val emptyCells = (0 until 9).flatMap { row ->
+            (0 until 9).map { col -> GridPoint(row, col) }
+        }.filter { it !in SolvedPuzzleData.INITIAL_FILLED }.shuffled()
+
+        delay(INITIAL_DELAY_MS)
+
+        for (index in emptyCells.indices) {
+            visibleCells = SolvedPuzzleData.INITIAL_FILLED + emptyCells.take(index + 1).toSet()
+            delay(CELL_DELAY_MS)
+        }
+    }
+
+    SolvedGridAnimation(
+        modifier = modifier,
+        initialFilled = SolvedPuzzleData.INITIAL_FILLED,
+        solvedGrid = SolvedPuzzleData.SOLVED_GRID,
+        visibleCells = visibleCells,
+    )
+}
