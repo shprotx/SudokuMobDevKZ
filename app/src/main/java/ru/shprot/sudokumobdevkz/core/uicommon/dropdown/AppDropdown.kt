dafd22b3d06121bc103515dbb3344489eab8babb
shprotx <shprot2x@gmail.com>
2026-07-29 22:31:23 +0500

    fix(#122): правки по ревью

--- app/src/main/java/ru/shprot/sudokumobdevkz/core/uicommon/dropdown/AppDropdown.kt
+++ app/src/main/java/ru/shprot/sudokumobdevkz/core/uicommon/dropdown/AppDropdown.kt
@@ -15,7 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
@@ -34,7 +34,7 @@ fun <T : DropdownItem> AppDropdown(
     placeholder: String = String.empty,
     enabled: Boolean = true,
 ) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
     LaunchedEffect(selected?.id) {
         expanded = false
